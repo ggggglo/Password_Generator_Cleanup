ab280f79bf08149271f43d7fc8a37118a615b4a9
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-08-16 11:55:10 -0500

    created if statements to choose if you want special characters, upper and or lowercase characters in your password

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var generateBtn = document.querySelector("#generate");
 
-var lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
-var upperCase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+var lower = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+var upper= ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
 var numeric = 1; 2; 3; 4; 5; 6; 7; 8; 9; 0;
 var characters = '\U+0020'; '\U+0021'; '\U+0022';
 
@@ -11,29 +11,48 @@ function createPassword (){
       window.confirm ("Ok! Onto the next!")
      } else if (length < 8 || length > 128) {
       window.prompt ("Error! Please enter a number between 8 and 128")
-      return;
-    } else {
-      window.prompt ("Error! Please enter a valid number")
-      return;
     }
   
   var numbers = window.prompt("Do you want numbers in your password? Please type yes or no");
-   if (numbers = "yes") {
+   if (numbers === "yes") {
     window.confirm ("Ok! Onto the next!")
-   } else if (numbers = "no") {
+   } else if (numbers === "no") {
    window.confirm ("Ok! Onto the next!")
    } else {
     window.prompt ("Please type yes or no")
-    return;
    }
+
+  var specialCharacters = window.prompt("Do you want special characters in your password? Please type yes or no");
+   if (specialCharacters === "yes") {
+    window.confirm ("Ok! Onto the next!")
+   } else if (specialCharacters === "no") {
+   window.confirm ("Ok! Onto the next!")
+  } else {
+    window.prompt ("Please type yes or no")
+    return;
   }
 
-  createPassword();
+  var upperCase = window.prompt("Do you want uppercase characters in your password?");
+    if (upperCase === "yes") {
+    window.confirm ("Ok! Onto the next!")
+    } else if (upperCase === "no") {
+    window.confirm ("Ok! Onto the next!")
+    } else {
+    window.prompt ("Please type yes or no")
+    return;
+  }
+  var lowerCase = window.prompt("Do you want lowercase characters in your password?");
+  if (lowerCase === "yes") {
+    window.confirm ("Ok! Onto the next!")
+   } else if (lowerCase === "no") {
+   window.confirm ("Ok! Ready to generate your password!")
+  } else {
+    window.prompt ("Please type yes or no")
+    return;
+  }
+}
 
- // var numbers = window.prompt("Do you want numbers in your password?");
-  //var select3 = window.prompt("Do you want special characters in your password?");
-  //var select4 = window.prompt("Do you want uppercase charactres in your password?");
- // var select5 = window.prompt("Do you want lowercase characters in your password?");
+createPassword();
 
 
 
@@ -48,4 +67,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
 
