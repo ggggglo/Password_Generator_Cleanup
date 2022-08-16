8a643bdd199ae3ad9cf082ddd33540bd17dad400
Gloriana Gonzalez <glogonzalez@Glorianas-MBP.lan>
2022-08-16 10:52:26 -0500

    created variables

---
+++ script.js
@@ -0,0 +1,12 @@
+// Assignment code here
+var lowerCase = ["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+var upperCase = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M"]
+var numeric = 1; 2; 3; 4; 5; 6; 7; 8; 9; 0;
+var characters = '\U+0020'; '\U+0021'; '\U+0022';
+
+
+  var select = window.prompt("Choose length of your password");
+  var select2 = window.prompt("Do you want numbers in your password?");
+  var select3 = window.prompt("Do you want special characters in your password?");
+  var select4 = window.prompt("Do you want uppercase charactres in your password?");
+  var select5 = window.prompt("Do you want lowercase characters in your password?");
